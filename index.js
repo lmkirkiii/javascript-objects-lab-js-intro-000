a5b317413a0895f445f7cf508567f40c256a863a
lmkirkiii@gmail.com Kirk <lmkirkiii@gmail.com>
2018-04-24 01:12:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function deleteFromObjectByKey(object, key){
 function deleteFromObjectByKey(object, key){
   var object
   var object2 = Object.assign({},object)
-  delete object2.key;
+  delete object2[key];
   return object2
 }
